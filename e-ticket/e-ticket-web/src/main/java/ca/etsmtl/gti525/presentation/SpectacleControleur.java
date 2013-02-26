f1c5daf9ba89330bbf8034e386cc37c50dfd707f
BILLEL <billel.redouane@gmail.com>
2013-02-26 04:28:30 +0000

    Ajout des beans Ajout des composant dans les pages fix bugs

---
+++ e-ticket/e-ticket-web/src/main/java/ca/etsmtl/gti525/presentation/SpectacleControleur.java
@@ -0,0 +1,19 @@
+package ca.etsmtl.gti525.presentation;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author REDOUANE
+ */
+@ManagedBean(name = "spectacleCtrl")
+@ViewScoped
+public class SpectacleControleur {
+
+    /**
+     * Creates a new instance of SpectacleControleur
+     */
+    public SpectacleControleur() {
+    }
+}
