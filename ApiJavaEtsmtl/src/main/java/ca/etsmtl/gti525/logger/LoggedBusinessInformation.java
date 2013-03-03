ac99ba082683a0ea5ae74b76fcaa8787e28285e6
BILLEL <billel.redouane@gmail.com>
2013-03-03 04:29:27 +0000

    Api ETSmtl + Les debut systeme de Logs

---
+++ ApiJavaEtsmtl/src/main/java/ca/etsmtl/gti525/logger/LoggedBusinessInformation.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.etsmtl.gti525.logger;
+
+/**
+ *
+ * @author Hadjout Dalil
+ */
+public interface LoggedBusinessInformation {
+    public String getLoggedBusinessInformation();
+
+    public void getSuccedMessage();
+
+   
+}
