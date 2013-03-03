d3a74973863ff1ff94a5076ab7ce8e74fec8b80b
billel.redouane@gmail.com <billel.redouane@gmail.com@38fecb63-1917-7c15-7e3d-f9ef088fe8c1>
2013-03-03 04:31:06 +0000

    Ajoute de la couche Persistance

---
+++ trunk/e-ticket-jpa/src/main/java/ca/etsmtl/gti525/entity/presentation/Representation.java
@@ -0,0 +1,120 @@
+package ca.etsmtl.gti525.entity.presentation;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ * @author REDOUANE
+ *
+ */
+@Entity
+@Table(name = "SHOW_REPRESENTATION")
+public class Representation implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+    @Column(name = "NB_BILLETS_DISPO")
+    private Integer nbBilletsDispo;
+    @Column(name = "PRIX")
+    private Float prix;
+    @Column(name = "NOM")
+    private String nom;
+    @Column(name = "ADRESSE")
+    private String adresse;
+    @Column(name = "DATE_DEBUT")
+    @Temporal(TemporalType.DATE)
+    private Date dateDebut;
+    @Column(name = "DATE_FIN")
+    @Temporal(TemporalType.DATE)
+    private Date dateFin;
+    @Column(name = "IS_ANNULATION")
+    private Boolean isAnnulation;
+                    
+    @ManyToOne
+    private Spectacle spectacle;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+  
+    public Spectacle getSpectacle() {
+        return spectacle;
+    }
+
+    public void setSpectacle(Spectacle spectacle) {
+        this.spectacle = spectacle;
+    }
+
+    public Integer getNbBilletsDispo() {
+        return nbBilletsDispo;
+    }
+
+    public void setNbBilletsDispo(Integer nbBilletsDispo) {
+        this.nbBilletsDispo = nbBilletsDispo;
+    }
+
+    public Float getPrix() {
+        return prix;
+    }
+
+    public void setPrix(Float prix) {
+        this.prix = prix;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public Date getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(Date dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public Date getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(Date dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public Boolean getIsAnnulation() {
+        return isAnnulation;
+    }
+
+    public void setIsAnnulation(Boolean isAnnulation) {
+        this.isAnnulation = isAnnulation;
+    }
+}
