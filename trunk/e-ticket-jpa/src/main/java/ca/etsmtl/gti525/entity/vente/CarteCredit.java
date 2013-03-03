aebd21bb2cebd178163cee5db030dd791844562d
billel.redouane@gmail.com <billel.redouane@gmail.com@38fecb63-1917-7c15-7e3d-f9ef088fe8c1>
2013-03-03 07:43:41 +0000

    Entity

--- trunk/e-ticket-jpa/src/main/java/ca/etsmtl/gti525/entity/vente/CarteCredit.java
+++ trunk/e-ticket-jpa/src/main/java/ca/etsmtl/gti525/entity/vente/CarteCredit.java
@@ -1,15 +1,38 @@
 package ca.etsmtl.gti525.entity.vente;
 
+import java.io.Serializable;
 import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
-public class CarteCredit {
-
-
+@Entity
+@Table(name="VENTE_CARTE_CREDIT")
+public class CarteCredit implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="ID")
+    private Long id;
+    @Column(name="NOM_DETENTEUR_CARTE")
     private String nomDetenteurCarte; //Nom du deteneur de la cart (pas besoin de mettre type Client => relation Unit drectionnel)
+    @Column(name="TYPE_CARTE_CREDIT")
     private String typeCarteCredit;
+    @Column(name="NUM_CARTE_CREDIT")
     private String numCarteCredit;
+    @Column(name="DATE_EXPIRATION")
+    @Temporal(TemporalType.DATE)
     private Date dateExpiration;
+    @Column(name="NUM_SECURITE")
     private String numSecurite; //G�n�ralement les 5 d�rnier chifre de la carte.
+    @Column(name="DATE_TRANSACTION")
+    @Temporal(TemporalType.DATE)
     private Date dateTransaction;
 
 //    private Client detenteurCarte;
