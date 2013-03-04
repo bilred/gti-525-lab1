d00ef06391512cf11537cff61702f61ad9644572
billel.redouane@gmail.com <billel.redouane@gmail.com@38fecb63-1917-7c15-7e3d-f9ef088fe8c1>
2013-03-04 06:58:36 +0000

    les relation v1 entre les tables

--- trunk/e-ticket-jpa/src/main/java/ca/etsmtl/gti525/entity/vente/Reservation.java
+++ trunk/e-ticket-jpa/src/main/java/ca/etsmtl/gti525/entity/vente/Reservation.java
@@ -1,5 +1,6 @@
 package ca.etsmtl.gti525.entity.vente;
 
+import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -8,52 +9,55 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
-@Table(name="SHOW_RESERVATION")
-public class Reservation  {
-	
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        @Column(name = "ID")
-        private String idReservation;
-           
-        @Column(name = "IS_RESERVATION")
-	private Boolean isReservation;
-        
-	@Column(name = "DATE_RESERVATION")
-    @Temporal(javax.persistence.TemporalType.DATE)
-        private Date dateReservation;
-        
-        @Column(name = "IS_CONFIRMATION_ACHAT")
-	private Boolean isConfirmationAchat;	
-	
-        public String getIdReservation() {
+@Table(name = "VENTE_RESERVATION")
+public class Reservation implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long idReservation;
+    @Column(name = "IS_RESERVATION")
+    private Boolean isReservation;
+    @Column(name = "DATE_RESERVATION")
+    @Temporal(TemporalType.DATE)
+    private Date dateReservation;
+    @Column(name = "IS_CONFIRMATION_ACHAT")
+    private Boolean isConfirmationAchat;
+
+    
+    public Long getIdReservation() {
         return idReservation;
-        }
+    }
 
-        public void setIdReservation(String idReservation) {
+    public void setIdReservation(Long idReservation) {
         this.idReservation = idReservation;
-        }
-	public Date getDateReservation() {
-		return dateReservation;
-	}
-	public void setDateReservation(Date dateReservation) {
-		this.dateReservation = dateReservation;
-	}
-	public Boolean getIsReservation() {
-		return isReservation;
-	}
-	public void setIsReservation(Boolean isReservation) {
-		this.isReservation = isReservation;
-	}
-	public Boolean getIsConfirmationAchat() {
-		return isConfirmationAchat;
-	}
-	public void setIsConfirmationAchat(Boolean isConfirmationAchat) {
-		this.isConfirmationAchat = isConfirmationAchat;
-	}
-	
-	
+    }
 
+    public Date getDateReservation() {
+        return dateReservation;
+    }
+
+    public void setDateReservation(Date dateReservation) {
+        this.dateReservation = dateReservation;
+    }
+
+    public Boolean getIsReservation() {
+        return isReservation;
+    }
+
+    public void setIsReservation(Boolean isReservation) {
+        this.isReservation = isReservation;
+    }
+
+    public Boolean getIsConfirmationAchat() {
+        return isConfirmationAchat;
+    }
+
+    public void setIsConfirmationAchat(Boolean isConfirmationAchat) {
+        this.isConfirmationAchat = isConfirmationAchat;
+    }
 }
