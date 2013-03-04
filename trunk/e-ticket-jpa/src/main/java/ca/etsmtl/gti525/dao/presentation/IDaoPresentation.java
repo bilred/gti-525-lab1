be77416797f7f1c0b02ae582e49632d10ec50f9e
billel.redouane@gmail.com <billel.redouane@gmail.com@38fecb63-1917-7c15-7e3d-f9ef088fe8c1>
2013-03-04 22:55:32 +0000

    domain Dao vente et presentation

---
+++ trunk/e-ticket-jpa/src/main/java/ca/etsmtl/gti525/dao/presentation/IDaoPresentation.java
@@ -0,0 +1,33 @@
+package ca.etsmtl.gti525.dao.presentation;
+
+
+import ca.etsmtl.gti525.entity.presentation.Artiste;
+import ca.etsmtl.gti525.entity.presentation.Representation;
+import ca.etsmtl.gti525.entity.presentation.Spectacle;
+import java.util.List;
+
+
+public interface IDaoPresentation {
+
+  // liste des clients
+  public List<Artiste> getAllArtistes();
+  // liste des Médecins
+  public List<Spectacle> getAllSpectacle();
+  public List<Representation> getAllRepresentation();
+//  // liste des créneaux horaires d'un médecin
+//  public List<Creneau> getAllCreneaux(Medecin medecin);
+//  // liste des Rv d'un médecin, un jour donné
+//  public List<Rv> getRvMedecinJour(Medecin medecin, Date jour);
+//  // trouver un client identifié par son id
+//  public Client getClientById(Long id);
+//  // trouver un client idenbtifié par son id
+//  public Medecin getMedecinById(Long id);
+//  // trouver un Rv identifié par son id
+//  public Rv getRvById(Long id);
+//  // trouver un créneau horaire identifié par son id
+//  public Creneau getCreneauById(Long id);
+//  // ajouter un RV
+//  public Rv ajouterRv(Date jour, Creneau creneau, Client client);
+//  // supprimer un RV
+//  public void supprimerRv(Rv rv);
+}
--- trunk/e-ticket-jpa/src/main/java/ca/etsmtl/gti525/dao/vente/IDaoVente.java
+++ trunk/e-ticket-jpa/src/main/java/ca/etsmtl/gti525/dao/vente/IDaoVente.java
@@ -1,19 +1,13 @@
-package ca.etsmtl.gti525.dao;
+package ca.etsmtl.gti525.dao.vente;
 
 
-import ca.etsmtl.gti525.entity.presentation.Artiste;
-import ca.etsmtl.gti525.entity.presentation.Representation;
-import ca.etsmtl.gti525.entity.presentation.Spectacle;
-import java.util.List;
+public interface IDaoVente {
 
-
-public interface IDao {
-
-  // liste des clients
-  public List<Artiste> getAllArtistes();
-  // liste des Médecins
-  public List<Spectacle> getAllSpectacle();
-  public List<Representation> getAllRepresentation();
+//  // liste des clients
+//  public List<Artiste> getAllArtistes();
+//  // liste des Médecins
+//  public List<Spectacle> getAllSpectacle();
+//  public List<Representation> getAllRepresentation();
 //  // liste des créneaux horaires d'un médecin
 //  public List<Creneau> getAllCreneaux(Medecin medecin);
 //  // liste des Rv d'un médecin, un jour donné
