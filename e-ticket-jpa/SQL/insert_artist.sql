6bb9782fd25e28468634d98f02d501d7ffada2a8
shonguiz@gmail.com <shonguiz@gmail.com>
2013-03-04 23:41:07 +0000

    presentation

---
+++ e-ticket-jpa/SQL/insert_artist.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO show_artiste(nom, description) VALUES ('CHRISTOPHER OWENS', 'description1');
+INSERT INTO show_artiste(nom, description) VALUES ('JOSH RITTER & THE ROYAL CITY BAND', 'description1');
+INSERT INTO show_artiste(nom, description) VALUES ('OMAR RODRIGUEZ LOPEZ GROUP', 'description1');
+INSERT INTO show_artiste(nom, description) VALUES ('REVIVAL', 'description1');
+INSERT INTO show_artiste(nom, description) VALUES ('THE MACCABEES', 'description1');
+INSERT INTO show_artiste(nom, description) VALUES ('WAKEY!WAKEY!', 'description1');
