f1aad6c184df8df901ac1d2c76748dce166943f5
BILLEL <billel.redouane@gmail.com>
2013-03-05 02:44:23 +0000

    fix bug scripte sql

--- e-ticket-jpa/SQL/insert_artist.sql
+++ e-ticket-jpa/SQL/insert_artist.sql
@@ -1,4 +1,4 @@
-﻿INSERT INTO show_artiste(nom, description) VALUES ('CHRISTOPHER OWENS', 'description1');
+INSERT INTO show_artiste(nom, description) VALUES ('CHRISTOPHER OWENS', 'description1');
 INSERT INTO show_artiste(nom, description) VALUES ('JOSH RITTER & THE ROYAL CITY BAND', 'description1');
 INSERT INTO show_artiste(nom, description) VALUES ('OMAR RODRIGUEZ LOPEZ GROUP', 'description1');
 INSERT INTO show_artiste(nom, description) VALUES ('REVIVAL', 'description1');
