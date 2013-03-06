10f6929489c9b9fb047ce43eb21669da54e2d999
shonguiz@gmail.com <shonguiz@gmail.com@38fecb63-1917-7c15-7e3d-f9ef088fe8c1>
2013-03-06 02:03:09 +0000

    presentation

--- trunk/e-ticket-jpa/src/main/java/ca/etsmtl/gti525/dao/presentation/ListsDataPresentation.java
+++ trunk/e-ticket-jpa/src/main/java/ca/etsmtl/gti525/dao/presentation/ListsDataPresentation.java
@@ -1,9 +1,29 @@
 package ca.etsmtl.gti525.dao.presentation;
 
+import ca.etsmtl.gti525.entity.presentation.Artiste;
+import ca.etsmtl.gti525.entity.presentation.Representation;
+import ca.etsmtl.gti525.entity.presentation.Spectacle;
+import java.util.List;
+
 /**
  *
  * @author REDOUANE
  */
-public class ListsDataPresentation {
+public class ListsDataPresentation implements IDaoPresentation{
+
+    @Override
+    public List<Artiste> getAllArtistes() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<Spectacle> getAllSpectacle() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<Representation> getAllRepresentation() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
     
 }
