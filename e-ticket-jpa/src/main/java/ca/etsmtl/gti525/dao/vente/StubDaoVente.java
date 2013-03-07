e54d5991e131b83017f33fb53f2bfe56cbf2b652
BILLEL <billel.redouane@gmail.com>
2013-03-07 04:34:35 +0000

    v.01 des batche en stubs

--- e-ticket-jpa/src/main/java/ca/etsmtl/gti525/dao/vente/StubDaoVente.java
+++ e-ticket-jpa/src/main/java/ca/etsmtl/gti525/dao/vente/StubDaoVente.java
@@ -4,25 +4,33 @@ import ca.etsmtl.gti525.entity.vente.Adresse;
 import ca.etsmtl.gti525.entity.vente.Client;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author REDOUANE
  */
 public class StubDaoVente implements IDaoVente {
+    
+    public List<Adresse> adresses = new ArrayList<Adresse>();
+    public List<Client> clients = new ArrayList<Client>();
 
-    public List<Adresse> Adresses = new ArrayList<Adresse>();
-    
-    
     @Override
     public void createAdresse(Adresse adresse) {
-        Adresses.add(adresse);
-        //System.out.println("Mon adresse"+adresse.getVille());
+        adresses.add(adresse);
+        Logger.getLogger(StubDaoVente.class.getName()).log(Level.INFO, null , "Creation des adresses.");
     }
 
     @Override
     public void createClient(Client client) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        clients.add(client);
+        Logger.getLogger(StubDaoVente.class.getName()).log(Level.INFO, null , "Creation des clients.");
     }
-    
+
+    @Override
+    public Adresse findAdresse(Long id) {
+        int index = id.intValue() - 1;
+        return adresses.get(index);
+    }
 }
