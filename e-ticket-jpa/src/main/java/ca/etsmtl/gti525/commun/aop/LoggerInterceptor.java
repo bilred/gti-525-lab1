168648c45668ecd046139030501e3af177460ded
BILLEL <billel.redouane@gmail.com>
2013-03-11 21:31:03 +0000

    init Dao (avec psedo syteme de logs)

--- e-ticket-jpa/src/main/java/ca/etsmtl/gti525/commun/aop/LoggerInterceptor.java
+++ e-ticket-jpa/src/main/java/ca/etsmtl/gti525/commun/aop/LoggerInterceptor.java
@@ -1,37 +1,37 @@
 package ca.etsmtl.gti525.commun.aop;
 
-import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 
-public class LoggerInterceptor implements MethodInterceptor
-{
-	@Override
-	public Object invoke(MethodInvocation methodInvocation) throws Throwable {
-		System.out.println("*************************");
-		System.out.println("Method name 1 : " + methodInvocation.getMethod().getName());
-		System.out.println("Method arguments 2 : " + Arrays.toString(methodInvocation.getArguments()));
+public class LoggerInterceptor implements MethodInterceptor {
 
-		//same with MethodBeforeAdvice
-		System.out.println("HijackAroundMethod 3 : Before method hijacked!");
-		
-		try{
-			//proceed to original method call
-			Object result = methodInvocation.proceed();
-                        
-                        System.out.println("*************************");
-			//same with AfterReturningAdvice
-			System.out.println("HijackAroundMethod 4 : Before after hijacked!");
-                        System.out.println("*************************");
-			return result;
-		
-		}catch(IllegalArgumentException e){
-			//same with ThrowsAdvice
-			System.out.println("HijackAroundMethod : Throw exception hijacked!");
-			throw e;
-		}
-		
-	}
-	
+    private static final Logger LOGGER = Logger.getLogger(LoggerInterceptor.class.toString());
+
+    @Override
+    public Object invoke(MethodInvocation methodInvocation) throws Throwable {
+//        System.out.println("************Debut du Log*************");
+        LOGGER.log(Level.INFO, "Before: {0}", methodInvocation.getMethod().getName());
+        
+        Object[] params = methodInvocation.getArguments();
+        if (params != null) {
+            for (int i = 0; i < params.length; i++) {
+                LOGGER.log(Level.INFO, "\n\tparamètre numéro " + i + " = " + params[i], methodInvocation.getMethod().getName());
+            }
+        }
+
+        try { //proceed to original method call
+            Object result = methodInvocation.proceed();  
+            return result; //same with AfterReturningAdvice
+
+        } catch (IllegalArgumentException ex) { //same with ThrowsAdvice
+            LOGGER.log(Level.SEVERE, "Execption s'est produit Encours d'execution", methodInvocation.getMethod().getName() + "----" + ex.getMessage());
+            throw ex;
+        } finally {
+            LOGGER.log(Level.INFO, "After:  {0}", methodInvocation.getMethod().getName());
+//            System.out.println("**********Fin du Log***************");
+        }
+    }
 }
