e2f6a5a7cf217bf591a877ce4390c39a79c8202a
shonguiz@gmail.com <shonguiz@gmail.com>
2013-03-12 01:15:56 +0000

    presentation

--- e-ticket/e-ticket-web/src/main/java/ca/etsmtl/gti525/commun/MenuControleur.java
+++ e-ticket/e-ticket-web/src/main/java/ca/etsmtl/gti525/commun/MenuControleur.java
@@ -3,11 +3,13 @@ package ca.etsmtl.gti525.commun;
 import ca.etsmtl.gti525.entity.presentation.Artiste;
 import ca.etsmtl.gti525.entity.presentation.Representation;
 import ca.etsmtl.gti525.entity.presentation.Spectacle;
+import ca.etsmtl.gti525.vente.crud.PanierBeans;
 import java.io.Serializable;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 import org.apache.log4j.Logger;
 
 /**
@@ -19,23 +21,43 @@ import org.apache.log4j.Logger;
 public class MenuControleur extends AbstractControleur implements Serializable {
     private static final Logger log = Logger.getLogger(ApplicationControleur.class);
     
+    private PanierBeans[] selectedRep;
+
+    public PanierBeans[] getSelectedRep() {
+        return selectedRep;
+    }
+
+    public void setSelectedRep(PanierBeans[] selectedRep) {
+        this.selectedRep = selectedRep;
+    }
+    
     // cache
     private List<Spectacle> spectacles;
     private List<Representation> representations;
+    private Spectacle specSelec = new Spectacle();
+
+    
     private List<Artiste> artistes;
-    
-    
     private List<Representation> representationsFiltrees;
+    private List lignesSelec;
+
+    public List getLignesSelec() {
+        return lignesSelec;
+    }
+
+    public void setLignesSelec(List lignesSelec) {
+        this.lignesSelec = lignesSelec;
+    }
 
     
     @PostConstruct
     public void init() {
         // instanciation couche [métier]
-        super.initPresentation();
+        super.initStubsPresentation();
 
-        this.spectacles = this.daoPresentation.getAllSpectacle();
-        this.representations = this.daoPresentation.getAllRepresentation();
-        this.artistes = this.daoPresentation.getAllArtistes(); 
+        this.spectacles = this.stubsDaoPresentation.getAllSpectacle();
+        this.representations = this.stubsDaoPresentation.getAllRepresentation();
+        this.artistes = this.stubsDaoPresentation.getAllArtistes(); 
         
         log.info("sonar source Spectacle 1: " + this.spectacles);
         log.info("sonar source Representation 1: " + this.representations);
@@ -50,7 +72,18 @@ public class MenuControleur extends AbstractControleur implements Serializable {
         this.representationsFiltrees = representationsFiltrees;
     }
     
+  
+    public String allerPageRep(List<Representation> rep){
+        
+                        
+        return "pageRepresentation?faces-redirect=true";
+    }
     
+    
+    public void onSelectionerOfEditer(Spectacle ss){
+        this.setSpecSelec(ss);
+        
+    }
     
     public String doHomme(){
         return "eticket.index";
@@ -67,6 +100,13 @@ public class MenuControleur extends AbstractControleur implements Serializable {
     /**
      * Creates a new instance of MenuControleur
      */
+    public Spectacle getSpecSelec() {
+        return specSelec;
+    }
+
+    public void setSpecSelec(Spectacle specSelec) {
+        this.specSelec = specSelec;
+    }
     public MenuControleur() {  }
 
     public List<Spectacle> getSpectacles() {
