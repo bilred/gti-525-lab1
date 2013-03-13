3450b82dbd56219d2e5fce634dfa1143718f7b70
billel.redouane@gmail.com <billel.redouane@gmail.com@38fecb63-1917-7c15-7e3d-f9ef088fe8c1>
2013-03-13 06:19:59 +0000

    correction du non du pakege

--- trunk/e-ticket/e-ticket-web/src/main/java/ca/etsmtl/gti525/vente/crud/TableCrudPanier.java
+++ trunk/e-ticket/e-ticket-web/src/main/java/ca/etsmtl/gti525/vente/crud/TableCrudPanier.java
@@ -3,7 +3,7 @@ package ca.etsmtl.gti525.vente.crud;
 import ca.etsmtl.gti525.vente.PanierControleur;
 import ca.etsmtl.gti525.commun.taglib.TableCrud;
 import ca.etsmtl.gti525.entity.presentation.Representation;
-import dz.elit.commun.transfert.PojoMap;
+import ca.etsmtl.utils.PojoMap;
 import java.io.Serializable;
 import java.util.List;
 import org.primefaces.event.SelectEvent;
