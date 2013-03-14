80c15ed2bc285069d0bfacce135a8e5a78d14ce9
billel.redouane@gmail.com <billel.redouane@gmail.com@38fecb63-1917-7c15-7e3d-f9ef088fe8c1>
2013-03-14 04:07:26 +0000

    avancement

---
+++ trunk/e-ticket-jpa/src/test/java/ca/etsmtl/gti525/dao/DataPaiementTest.java
@@ -0,0 +1,55 @@
+package ca.etsmtl.gti525.dao;
+
+import ca.etsmtl.gti525.dao.vente.IDaoVente;
+import gti525.paiement.IPaiementDAO;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ * @author REDOUANE
+ */
+public class DataPaiementTest extends AbstractDaoJpaTest {
+
+    public DataPaiementTest() { }
+    // couche [dao] testée
+    protected static IPaiementDAO stubsDaoJpaPaiement;
+    //protected static IDaoVente stubsDaoVente;
+
+
+    @BeforeClass
+    public static void init() {
+        // instanciation couche [dao]
+        ApplicationContext ctx = new ClassPathXmlApplicationContext(new String[] {"spring-config-data.xml"});
+        stubsDaoJpaPaiement = (IPaiementDAO) ctx.getBean("stubsDaoJpaPaiement");        
+    }
+
+    /**
+     * Test of getAllArtistes method, of class DaoJpa.
+     */
+    @Test
+    public void testGetAllArtistes() { // affichage Data
+//        List<Client> client = stubsDaoVente.getAllCleint();
+//        display("Liste des client :", client); 
+//        Assert.assertTrue("si table null alors True :", this.equals(client) );
+    }
+
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+    
+}
