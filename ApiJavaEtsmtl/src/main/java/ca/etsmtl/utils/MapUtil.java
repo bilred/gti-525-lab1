59b3e37a21018fea4d01612c0e5b0b576ba49e84
BILLEL <billel.redouane@gmail.com>
2013-03-14 23:12:32 +0000

    avancement

--- ApiJavaEtsmtl/src/main/java/ca/etsmtl/utils/MapUtil.java
+++ ApiJavaEtsmtl/src/main/java/ca/etsmtl/utils/MapUtil.java
@@ -10,7 +10,7 @@ import java.util.Map;
  * Utilisé pour fair le transfaire d'une List en Map.<br/>
  * Ou dans mon cas pour Simulé une Collection de List (besoin JSF => une p:collector balise qui prend pas en parramétre des Map)<br/><br/>
  * <br/>
- * PojoMap == une repréantation simplifier d'une Map(Clés, Valeur)
+ * MapUtil == une repréantation simplifier d'une Map(Clés, Valeur)
  * 
  * Condition : la taille des deux liste doit étre egale (pour respet du pricipe des Map(clés, Valeur))<br/><br/>
  * 
@@ -19,14 +19,14 @@ import java.util.Map;
  * 
  * @author redouane.billel
  */
-public class PojoMap<KeyType, ValueType> {
+public class MapUtil<KeyType, ValueType> {
     
     private KeyType key;
     private ValueType valeur;
 
     
-    public PojoMap() { }
-    public PojoMap(KeyType key, ValueType valeur) {
+    public MapUtil() { }
+    public MapUtil(KeyType key, ValueType valeur) {
         this.key = key;
         this.valeur = valeur;
     }
@@ -38,44 +38,44 @@ public class PojoMap<KeyType, ValueType> {
 //    }
     
     /**
-     * Permet la transformation d'une PojoMap (du type de cette classe) a une List.<br/><br/>
+     * Permet la transformation d'une MapUtil (du type de cette classe) a une List.<br/><br/>
      * 
      * Utilisé dans le but d'extraire d'une map les valeurs Clés mais sous une forme de List Java.<br/>
      * 
      * @param list
      * @return 
      */
-    public List<KeyType> getListKeyOfPojoMap(List<PojoMap<KeyType, ValueType>> list){
+    public List<KeyType> getListKeyOfPojoMap(List<MapUtil<KeyType, ValueType>> list){
         List<KeyType> listKey = new ArrayList<KeyType>();    
-        for(PojoMap<KeyType, ValueType> pojoAgenceBancaire : list)
+        for(MapUtil<KeyType, ValueType> pojoAgenceBancaire : list)
             listKey.add( pojoAgenceBancaire.key );    
  
         return listKey;
     }
     
     /**
-     * Permet la transformation d'une PojoMap (du type de cette classe) a une List.<br/><br/>
+     * Permet la transformation d'une MapUtil (du type de cette classe) a une List.<br/><br/>
      * 
      * Utilisé dans le but d'extraire d'une map les valeurs (sans les Clés) mais sous une forme de List Java.<br/>
      * 
      * @param list
      * @return 
      */
-    public List<ValueType> getListValeurOfPojoMap(List<PojoMap<KeyType, ValueType>> list){
+    public List<ValueType> getListValeurOfPojoMap(List<MapUtil<KeyType, ValueType>> list){
         List<ValueType> listValeur = new ArrayList<ValueType>();    
-        for(PojoMap<KeyType, ValueType> pojoAgenceBancaire : list)
+        for(MapUtil<KeyType, ValueType> pojoAgenceBancaire : list)
             listValeur.add( pojoAgenceBancaire.valeur );     
  
         return listValeur;        
     } 
     
     /**
-     * Permet la transformation d'une PojoMap a une Map JAVA.
+     * Permet la transformation d'une MapUtil a une Map JAVA.
      * 
      * @param pojoMaps
      * @return 
      */
-    public Map<KeyType, ValueType> getMap(List<PojoMap<KeyType, ValueType>> pojoMaps){
+    public Map<KeyType, ValueType> getMap(List<MapUtil<KeyType, ValueType>> pojoMaps){
         Map<KeyType, ValueType> maps = new HashMap<KeyType, ValueType>();
         for (int i=0; i<pojoMaps.size(); i++) 
             maps.put(pojoMaps.get(i).key, pojoMaps.get(i).valeur);
@@ -83,16 +83,16 @@ public class PojoMap<KeyType, ValueType> {
     }    
     
     /**
-     * Transforme une Map a une List de PojoMap.<br/>
+     * Transforme une Map a une List de MapUtil.<br/>
      * l'ide viens du JAVA: Integer.valeuOf , ....
      * 
      * @param maps
      * @return 
      */
-    public List<PojoMap<KeyType, ValueType>> getPojoMap(Map<KeyType, ValueType> maps){
-        List<PojoMap<KeyType,ValueType>> pojoMap = new ArrayList<PojoMap<KeyType, ValueType>>(); 
+    public List<MapUtil<KeyType, ValueType>> getPojoMap(Map<KeyType, ValueType> maps){
+        List<MapUtil<KeyType,ValueType>> pojoMap = new ArrayList<MapUtil<KeyType, ValueType>>(); 
         
-         List listk = PojoMap.transformeArrayToList( maps.keySet().toArray());
+         List listk = MapUtil.transformeArrayToList( maps.keySet().toArray());
          pojoMap.addAll( listk );
          List listv = (List) maps.values();
          pojoMap.addAll( listv );            
@@ -117,20 +117,20 @@ public class PojoMap<KeyType, ValueType> {
     }    
 
     /**
-     * Transforme une Map a une List de PojoMap.<br/> (d'oublant de méthode)
+     * Transforme une Map a une List de MapUtil.<br/> (d'oublant de méthode)
      * 
      * @param map
      * @return 
      */
-    public List<PojoMap<KeyType, ValueType>> convertMapToListPojo(Map<?,?> map) {
-        List<PojoMap<KeyType, ValueType>> list = new ArrayList<PojoMap<KeyType, ValueType>>();
+    public List<MapUtil<KeyType, ValueType>> convertMapToListPojo(Map<?,?> map) {
+        List<MapUtil<KeyType, ValueType>> list = new ArrayList<MapUtil<KeyType, ValueType>>();
         List<KeyType> listKeys = new ArrayList<KeyType>(); List<ValueType> listValeurs = new ArrayList<ValueType>();
         
-        listKeys = (List<KeyType>) PojoMap.transformeArrayToList( map.keySet().toArray() );
-        listValeurs = (List<ValueType>) PojoMap.transformeArrayToList( map.values().toArray() );
+        listKeys = (List<KeyType>) MapUtil.transformeArrayToList( map.keySet().toArray() );
+        listValeurs = (List<ValueType>) MapUtil.transformeArrayToList( map.values().toArray() );
         
         for(int i = 0; i < listKeys.size(); i++) {
-           list.add( new PojoMap<KeyType, ValueType>(listKeys.get(i), listValeurs.get(i))  ); 
+           list.add( new MapUtil<KeyType, ValueType>(listKeys.get(i), listValeurs.get(i))  ); 
         }
         
         return list;
