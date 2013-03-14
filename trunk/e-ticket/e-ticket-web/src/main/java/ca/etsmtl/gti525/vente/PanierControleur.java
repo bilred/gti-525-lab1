ec55512a14b9f0ff21003efe79f7547d6f79fb8b
billel.redouane@gmail.com <billel.redouane@gmail.com@38fecb63-1917-7c15-7e3d-f9ef088fe8c1>
2013-03-14 04:08:31 +0000

    avancement

--- trunk/e-ticket/e-ticket-web/src/main/java/ca/etsmtl/gti525/vente/PanierControleur.java
+++ trunk/e-ticket/e-ticket-web/src/main/java/ca/etsmtl/gti525/vente/PanierControleur.java
@@ -3,10 +3,10 @@ package ca.etsmtl.gti525.vente;
 import ca.etsmtl.gti525.commun.taglib.TableCrud;
 import ca.etsmtl.gti525.entity.presentation.Representation;
 import ca.etsmtl.gti525.presentation.CacheSessionPresentation;
-import ca.etsmtl.gti525.presentation.RepresentationControleur;
 import ca.etsmtl.gti525.vente.crud.PanierBeans;
 import ca.etsmtl.gti525.vente.crud.TableCrudPanier;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
@@ -22,22 +22,35 @@ import org.apache.log4j.Logger;
 public class PanierControleur implements Serializable {    
     private static final Logger log = Logger.getLogger(PanierControleur.class);
 
-    //pour recuprér les represantation selectionner (et remplire le panier)
+    //pour recuprér les represantation selectionner (et remplire le paniers)
     @ManagedProperty(value = "#{cacheSessionPresentationCtrl}")
     private CacheSessionPresentation cacheSessionPresentation;    
        
     private TableCrud tableCrudPanier;
-
+    private List<PanierBeans> paniers; //passer au proccese de paiment
     
-    
+    //private Representation representationDetail;
     public void onDetailsEnregistrement(Representation item){
         
     }
     
     private int count;
     public void increment() {
+        PanierBeans panier = null;
+        List<Representation> repSelect = getCacheSessionPresentation().getRepresentationSelected();
+        
+        for(Representation rep: repSelect) {  
+         panier = new PanierBeans();   
+         panier.setNomSpectacle( rep.getNom() );
+         panier.setPrix( rep.getPrix() );
+         panier.setQuantity( repSelect.size() );
+         panier.setVille( rep.getAdresse() );
+         
+         this.getPanier().add(panier);
+        }
+        
         log.info("Appel de la méthode increment(), valeur initial: "+count);
-        count++;  
+        count++; //selon le nombre de biller  
     }  
     
     
@@ -77,5 +90,17 @@ public class PanierControleur implements Serializable {
     public void setCacheSessionPresentation(CacheSessionPresentation cacheSessionPresentation) {
         this.cacheSessionPresentation = cacheSessionPresentation;
     }
+
+    public List<PanierBeans> getPanier() {
+        if(paniers==null) paniers = new ArrayList<PanierBeans>();
+        return paniers;
+    }
+
+    public void setPanier(List<PanierBeans> panier) {
+        if(paniers==null) paniers = new ArrayList<PanierBeans>();
+        this.paniers = panier;
+    }
+    
+    
         
 }
