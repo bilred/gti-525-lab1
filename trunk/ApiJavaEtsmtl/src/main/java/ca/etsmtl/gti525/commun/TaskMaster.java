68e19ee2afd061930fe2da2231bd0a4e2fd9eee0
shonguiz@gmail.com <shonguiz@gmail.com@38fecb63-1917-7c15-7e3d-f9ef088fe8c1>
2013-03-17 01:57:11 +0000

    model mise a jour

---
+++ trunk/ApiJavaEtsmtl/src/main/java/ca/etsmtl/gti525/commun/TaskMaster.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.etsmtl.gti525.commun;
+
+/**
+ *
+ * @author shonguiz
+ */
+import java.util.TimerTask;
+
+import java.util.Timer;
+
+public class TaskMaster extends TimerTask{
+
+
+
+
+       public void run(){
+
+           System.out.println("Test" );
+
+       }
+}
+
