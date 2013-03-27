ccd43cb0e7b153b8bf1ebb994236629b7b90c294
BILLEL <billel.redouane@gmail.com>
2013-03-27 06:46:23 +0000

    j'ai ajouter la requet JPQL findRepresentation(Long id)

--- e-ticket-jpa/src/main/java/ca/etsmtl/gti525/dao/presentation/DaoJpaPresentation.java
+++ e-ticket-jpa/src/main/java/ca/etsmtl/gti525/dao/presentation/DaoJpaPresentation.java
@@ -8,50 +8,50 @@ import ca.etsmtl.gti525.entity.presentation.Spectacle;
 import ca.etsmtl.gti525.entity.vente.Reservation;
 import java.io.Serializable;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import org.springframework.transaction.annotation.Transactional;
-
 
 @Transactional
 public class DaoJpaPresentation implements IDaoPresentation, Serializable {
 
-  private static final long serialVersionUID = 1L;
-  @PersistenceContext
-  private EntityManager em;
+    private static final long serialVersionUID = 1L;
+    @PersistenceContext
+    private EntityManager em;
 
-  // liste des clients
+    // liste des clients
     @Override
-  public List<Artiste> getAllArtistes() {
-    try {
-      return em.createQuery("select rc from Artiste rc").getResultList();
-    } catch (Throwable th) {
-      //throw new RdvMedecinsException(th, 1);
-        return null;
+    public List<Artiste> getAllArtistes() {
+        try {
+            return em.createQuery("select rc from Artiste rc").getResultList();
+        } catch (Throwable th) {
+            //throw new RdvMedecinsException(th, 1);
+            return null;
+        }
     }
-  }
 
- 
     @Override
-  public List<Spectacle> getAllSpectacle() {
-    try {
-      return em.createQuery("select rm from Spectacle rm").getResultList();
-    } catch (Throwable th) {
-      //throw new RdvMedecinsException(th, 2);
-        return null;
+    public List<Spectacle> getAllSpectacle() {
+        try {
+            return em.createQuery("select rm from Spectacle rm").getResultList();
+        } catch (Throwable th) {
+            //throw new RdvMedecinsException(th, 2);
+            return null;
+        }
     }
-  }
-  
- 
+
     @Override
-  public List<Representation> getAllRepresentation() {
-    try {
-      return em.createQuery("select rm from Representation rm").getResultList();
-    } catch (Throwable th) {
-      //throw new RdvMedecinsException(th, 2);
-        return null;
+    public List<Representation> getAllRepresentation() {
+        try {
+            return em.createQuery("select rm from Representation rm").getResultList();
+        } catch (Throwable th) {
+            //throw new RdvMedecinsException(th, 2);
+            return null;
+        }
     }
-  }
 
     @Override
     public void createBillet(Billet billet) {
@@ -60,7 +60,16 @@ public class DaoJpaPresentation implements IDaoPresentation, Serializable {
 
     @Override
     public Representation findRepresentation(Long id) {
-        return null;
+        try {
+            Query query;
+            query = (Query) em.createQuery("SELECT a FROM Representation a  where a.id=:id");
+            query.setParameter("id", id);
+
+            return ((Representation) query.getSingleResult());
+        } catch (Exception ex) {
+            Logger.getLogger(DaoJpaPresentation.class.toString()).log(Level.SEVERE, ex.toString());
+            return null;
+        }
     }
 
     @Override
@@ -92,10 +101,9 @@ public class DaoJpaPresentation implements IDaoPresentation, Serializable {
     public void createArtist(Artiste artist) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-    
+
     @Override
     public Representation findRepresentationByID(long id) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-    
 }
