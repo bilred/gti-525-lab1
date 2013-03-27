12f784e99fa38905cfb0ad217a78ab7306149f49
shonguiz@gmail.com <shonguiz@gmail.com>
2013-03-27 18:49:23 +0000

    rep

--- e-ticket-jpa/src/main/resources/sql/insert_representation.sql
+++ e-ticket-jpa/src/main/resources/sql/insert_representation.sql
@@ -1,18 +1,83 @@
-INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-02-26 09:00:00', 100, 25, 2, 1);
-INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-02-27 08:00:00', 100, 25, 1, 1);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-02-26 09:00:00', 100, 25, 1, 1);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-02-27 08:00:00', 100, 25, 2, 1);
 INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-02-28 10:00:00', 100, 25, 3, 1);
-INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-01 11:00:00', 100, 25, 3, 1);
-INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 12:00:00', 100, 25, 2, 1);
-INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 100, 25, 3, 1);
-INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 100, 25, 4, 2);
-INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-27 09:00:00', 100, 25, 1, 2);
-INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 100, 25, 2, 2);
-INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 100, 25, 5, 2);
-INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 90, 20, 3, 2);
-INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 80, 20, 2, 3);
-INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-31 09:00:00', 70, 15, 1, 3);
-INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 60, 35, 1, 3);
-INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 08:00:00', 100, 25, 5, 3);
-INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 100, 25, 4, 3);
-INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 100, 25, 2, 3);
-INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 05:00:00', 100, 25, 3, 3);
+
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-01 11:00:00', 100, 25, 4, 2);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 12:00:00', 100, 25, 5, 2);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 100, 25, 6, 2);
+
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-01 11:00:00', 100, 25, 7, 3);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 12:00:00', 100, 25, 8, 3);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 100, 25, 9, 3);
+
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 100, 25, 1, 4);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-27 09:00:00', 100, 25, 2, 4);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 100, 25, 3, 4);
+
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-01 11:00:00', 100, 25, 4, 5);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 12:00:00', 100, 25, 5, 5);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 100, 25, 6, 5);
+
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-01 11:00:00', 100, 25, 7, 6);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 12:00:00', 100, 25, 8, 6);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 100, 25, 9, 6);
+
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-01 11:00:00', 100, 25, 1, 7);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 12:00:00', 100, 25, 2, 7);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 100, 25, 3, 7);
+
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-01 11:00:00', 100, 25, 4, 8);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 12:00:00', 100, 25, 5, 8);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 100, 25, 6, 8);
+
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-01 11:00:00', 100, 25, 7, 9);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 12:00:00', 100, 25, 8, 9);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 100, 25, 9, 9);
+
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-01 11:00:00', 100, 25, 1, 10);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 12:00:00', 100, 25, 2, 10);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 100, 25, 3, 10);
+
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-01 11:00:00', 100, 25, 4, 11);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 12:00:00', 100, 25, 5, 11);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 100, 25, 6, 11);
+
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-01 11:00:00', 100, 25, 7, 12);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 12:00:00', 100, 25, 8, 12);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 100, 25, 9, 12);
+
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-01 11:00:00', 100, 25, 1, 13);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 12:00:00', 100, 25, 2, 13);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 100, 25, 3, 13);
+
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-01 11:00:00', 100, 25, 4, 14);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 12:00:00', 100, 25, 5, 14);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 100, 25, 6, 14);
+
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-01 11:00:00', 100, 25, 7, 15);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 12:00:00', 100, 25, 8, 15);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 100, 25, 9, 15);
+
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-01 11:00:00', 100, 25, 1, 16);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 12:00:00', 100, 25, 2, 16);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 100, 25, 3, 16);
+
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-01 11:00:00', 100, 25, 4, 17);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 12:00:00', 100, 25, 5, 17);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 100, 25, 6, 17);
+
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-01 11:00:00', 100, 25, 4, 18);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 12:00:00', 100, 25, 5, 18);
+INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 100, 25, 6, 18);
+
+--INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 100, 25, 5, 2);
+
+--INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 90, 20, 3, 2);
+
+--INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 80, 20, 2, 3);
+--INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-31 09:00:00', 70, 15, 1, 3);
+--INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 60, 35, 1, 3);
+--INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 08:00:00', 100, 25, 5, 3);
+--INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 100, 25, 4, 3);
+--INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 09:00:00', 100, 25, 2, 3);
+--INSERT INTO show_representation(DATE_DEBUT, nb_billets_dispo, prix, salle_id, spectacle_id)VALUES ('2013-03-26 05:00:00', 100, 25, 3, 3);
