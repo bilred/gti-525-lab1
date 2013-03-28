2620e9227f580fc5c011cf3a7dc1899c4b60062c
shonguiz@gmail.com <shonguiz@gmail.com@38fecb63-1917-7c15-7e3d-f9ef088fe8c1>
2013-03-28 02:54:37 +0000

    rep

--- trunk/e-ticket/e-ticket-web/src/main/java/ca/etsmtl/gti525/vente/ProcessPaiementControleur.java
+++ trunk/e-ticket/e-ticket-web/src/main/java/ca/etsmtl/gti525/vente/ProcessPaiementControleur.java
@@ -3,11 +3,15 @@ package ca.etsmtl.gti525.vente;
 import ca.etsmtl.gti525.beans.paiement.PanierBeans;
 import ca.etsmtl.gti525.commun.AbstractControleur;
 import ca.etsmtl.gti525.commun.CommunService;
+import ca.etsmtl.gti525.dao.paiement.PaiementDAO;
 import ca.etsmtl.gti525.entity.vente.CarteCredit;
 import ca.etsmtl.gti525.entity.vente.Client;
+import ca.etsmtl.utils.DateUtils;
 import gti525.paiement.InformationsPaiementTO;
+import gti525.paiement.ReponseSystemePaiementTO;
 import gti525.paiement.RequeteAuthorisationTO;
 import java.io.Serializable;
+import java.math.BigDecimal;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.faces.bean.ManagedBean;
@@ -27,6 +31,9 @@ public class ProcessPaiementControleur extends AbstractControleur implements Ser
     private static Logger logger = Logger.getLogger(ProcessPaiementControleur.class.getName());
     
     //pour recuprér les represantation selectionner (et remplire le panier)
+    private static final String API_KEY = "38f33d8449386908efd4";
+    private static final long CARD_NUMBER = 8675309000000000l;
+    private static final int STORE_ID = 55; 
     @ManagedProperty(value = "#{panierCtrl}")
     private PanierControleur panierCtrl;    
       
@@ -76,18 +83,47 @@ public class ProcessPaiementControleur extends AbstractControleur implements Ser
     public String onFlowProcess(FlowEvent event) {  
         logger.info("Current wizard step:" + event.getOldStep());  
         logger.info("Next step:" + event.getNewStep());  
-          
+        InformationsPaiementTO informationsPaiementTO =  new InformationsPaiementTO();
+        PaiementDAO pay = new PaiementDAO();
+        ReponseSystemePaiementTO preReponse = new ReponseSystemePaiementTO();
+        informationsPaiementTO.setApi_key("38f33d8449386908efd4");
+        informationsPaiementTO.setOrder_id(50);
+        informationsPaiementTO.setStore_id(50);
+        informationsPaiementTO.setMonth(10);
+        informationsPaiementTO.setYear(2016);
+        
+        informationsPaiementTO.setSecurity_code(333);
         if(event.getNewStep().equals("client")) { //step creditCard
+
             CommunService.addInfo("Succes", "Next step: " + event.getNewStep() );
         }
         if(event.getNewStep().equals("creditCard")) { //step creditCard
+            informationsPaiementTO.setFirst_name( "tata");
+            informationsPaiementTO.setLast_name("zaza");
+            informationsPaiementTO.setAmount(BigDecimal.TEN);
+            
+            
+           
+            
             CommunService.addInfo("Succes", "Next step: " + event.getNewStep() );
         }
         if (event.getNewStep().equals("confirm")) { //step client 
-           InformationsPaiementTO infoPaiement = new InformationsPaiementTO(); infoPaiement.setCard_number(202215848911L); //...
+           informationsPaiementTO.setCard_number(8675309000000000l);
+           
+           //informationsPaiementTO.setMonth(DateUtils.getMonth(this.carteCredit.getDateExpiration()));
+           //informationsPaiementTO.setYear(DateUtils.getYear(this.carteCredit.getDateExpiration()));
+           
+           preReponse=pay.effectuerPreauthorisation(informationsPaiementTO);
+           String statut = preReponse.getStatus();
+           
+           System.out.print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"+preReponse.getStatus()+"ZZZZZZZZZZZZZZZZZZZZ");
+           //...
            //ReponseSystemePaiementTO reponseSystemePaiementTO = InitDao.stubsDaoJpaPaiement.effectuerPreauthorisation(infoPaiement);
            //reponseSystemePaiementTO
-           CommunService.addInfo("Succes", "Méthode «effectuerPreauthorisation();»");
+           //if (statut.equals("Accepted")){
+               CommunService.addInfo(statut, "Méthode «effectuerPreauthorisation();»");
+           //}
+           
         }
 
         if("undefined".equals(event.getNewStep())) return "tickets";
