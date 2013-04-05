c54be3b992ece84b2031e47c764cbf1c291c209d
billel.redouane@gmail.com <billel.redouane@gmail.com@38fecb63-1917-7c15-7e3d-f9ef088fe8c1>
2013-04-05 22:49:21 +0000

    mis en forme SQL

--- trunk/e-ticket-jpa/src/main/resources/sql/insert_salle.sql
+++ trunk/e-ticket-jpa/src/main/resources/sql/insert_salle.sql
@@ -1,11 +1,9 @@
 INSERT INTO show_salle (adresse, nb_place, NAME) VALUES ('Montreal', 30000, 'Centre Bell');
 INSERT INTO show_salle (adresse, nb_place, NAME) VALUES ('Ontario', 30000, 'Ontario Hall');
 INSERT INTO show_salle (adresse, nb_place, NAME) VALUES ('Vancouver', 30000, 'Vancouver Mega-Center');
-
 INSERT INTO show_salle (adresse, nb_place, NAME) VALUES ('New York', 60000, 'The World');
 INSERT INTO show_salle (adresse, nb_place, NAME) VALUES ('Washington', 40000, 'Capitol');
 INSERT INTO show_salle (adresse, nb_place, NAME) VALUES ('San Fransisco', 40000, 'The Heaven');
-
 INSERT INTO show_salle (adresse, nb_place, NAME) VALUES ('Paris', 60000, 'Zenith');
 INSERT INTO show_salle (adresse, nb_place, NAME) VALUES ('Berlin', 50000, 'Berghain');
 INSERT INTO show_salle (adresse, nb_place, NAME) VALUES ('Mouscou', 30000, 'Opera');
