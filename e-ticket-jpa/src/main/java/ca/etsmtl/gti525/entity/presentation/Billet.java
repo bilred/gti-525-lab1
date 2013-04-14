0159930e5db7aaee2d407aacc409cb6c20ea5ed9
shonguiz@gmail.com <shonguiz@gmail.com>
2013-04-14 00:54:24 +0000

    rep

--- e-ticket-jpa/src/main/java/ca/etsmtl/gti525/entity/presentation/Billet.java
+++ e-ticket-jpa/src/main/java/ca/etsmtl/gti525/entity/presentation/Billet.java
@@ -2,6 +2,7 @@ package ca.etsmtl.gti525.entity.presentation;
 
 import ca.etsmtl.gti525.entity.vente.Reservation;
 import java.io.Serializable;
+import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
 
 /**
  * Un Billet représant un accés a une salle. (avec une date Debut et Fin == Date
@@ -28,6 +30,14 @@ public class Billet implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID")
     private Long idBillet;
+    @Column(name="ADRESSE")
+    private String adresse;
+    @Column(name="DATE_REPRESENTATION")
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dateRepresentation;
+    @Column(name="SPECTACLE")
+    private String nomSpec;
+    
 //    @Column(name = "IS_VENDU")
 //    private Boolean isVendu;  //enVente; ou vendu.
 //    @Column(name = "IS_LIVRAISON")
@@ -37,9 +47,33 @@ public class Billet implements Serializable {
     private Salle salle; //pour avoir des information comme Num�ro de Siege.   
     @OneToOne
     private Representation representation; //en les recup�re d'ici : private Date dateDebut; private Date dateFin;
-    @OneToOne
-    private Reservation reservation;  //-les 10 ou 20 1er minut de la connexion Client -s'il est vendu il est reserv� a vie
+   // @OneToOne
+   // private Reservation reservation;  //-les 10 ou 20 1er minut de la connexion Client -s'il est vendu il est reserv� a vie
                                       //Reservation == c'est la seul interface avec le module Vente de l'Application	
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public Date getDateRepresentation() {
+        return dateRepresentation;
+    }
+
+    public void setDateRepresentation(Date dateRepresentation) {
+        this.dateRepresentation = dateRepresentation;
+    }
+
+    public String getNomSpec() {
+        return nomSpec;
+    }
+
+    public void setNomSpec(String nomSpec) {
+        this.nomSpec = nomSpec;
+    }
 
     
     public Long getIdBillet() {
@@ -58,13 +92,13 @@ public class Billet implements Serializable {
         this.representation = representation;
     }
 
-    public Reservation getReservation() {
+   /* public Reservation getReservation() {
         return reservation;
     }
 
     public void setReservation(Reservation reservation) {
         this.reservation = reservation;
-    }
+    }*/
 
 //    public Boolean getIsVendu() {
 //        return isVendu;
